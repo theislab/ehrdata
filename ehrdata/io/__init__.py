15af496c5d3b700edab8b5baeb6f446a9f685567
Eljas Roellin <eljas.roellin@ikmail.com>
2024-10-03 23:44:13 +0200

    update commit

--- ehrdata/io/__init__.py
+++ ehrdata/io/__init__.py
@@ -1,13 +1,1 @@
-from ehrdata.io._omop import (
-    extract_condition_occurrence,
-    extract_device_exposure,
-    extract_measurement,
-    extract_note,
-    extract_observation,
-    extract_observation_period,
-    extract_person,
-    extract_procedure_occurrence,
-    extract_specimen,
-    load,
-    time_interval_table,
-)
+from ehrdata.io import omop
