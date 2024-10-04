7ebdd4e4d9d65104c9aff3ffb821fa8139a590b2
Eljas Roellin <eljas.roellin@ikmail.com>
2024-10-04 15:09:44 +0200

    update dataset loading

--- src/ehrdata/dt/datasets.py
+++ src/ehrdata/dt/datasets.py
@@ -1,19 +1,15 @@
-# maybe download csv for all of them;
-# and then can do ep.io.extract_omop(...)
-# or duckdb.connect_or_so_to_csvs_in_directory(paths)
-
-# decide on api; connect vs load vs extract..
-
 import io
 import os
 import zipfile
+from pathlib import Path
 
 import requests
+from duckdb.duckdb import DuckDBPyConnection
 
 from ehrdata.utils._omop_utils import get_table_catalog_dict
 
 
-def _get_table_list():
+def _get_table_list() -> list:
     flat_table_list = []
     for _, value_list in get_table_catalog_dict().items():
         for value in value_list:
@@ -21,7 +17,7 @@ def _get_table_list():
     return flat_table_list
 
 
-def _set_up_duckdb(path, backend_handle):
+def _set_up_duckdb(path: Path, backend_handle: DuckDBPyConnection) -> None:
     tables = _get_table_list()
 
     missing_tables = []
@@ -40,32 +36,34 @@ def _set_up_duckdb(path, backend_handle):
     print("missing tables: ", missing_tables)
 
 
-def mimic_iv_omop(backend="duckdb", backend_handle=None) -> None:
+def mimic_iv_omop(backend_handle: DuckDBPyConnection, data_path: Path | None = None) -> None:
     """Loads the MIMIC-IV demo data in the OMOP Common Data model.
 
-    More details: https://physionet.org/content/mimic-iv-demo-omop/0.9/#files-panel
+    More details: https://physionet.org/content/mimic-iv-demo-omop/0.9/#files-panel.
 
-    Args:
-        backend: What backend shall be used to load and handle the tables.
-        Currently, only "duckdb" is supported.
-        backend_handle: A handle to the backend which shall be used. Should be a duckdb connection.
+    Parameters
+    ----------
+        backend_handle: A handle to the backend which shall be used. Only duckdb connection supported at the moment.
+        data_path: Path to the tables. If the path exists, the data is loaded from there. Else, the data is downloaded.
 
     Returns
     -------
-        None. Maybe a report in the future
+    Returns nothing, but adds the tables to the backend via the handle.
 
     Examples
     --------
         >>> import ehrapy as ep
         >>> import ehrdata as ed
         >>> import duckdb
-        >>> conn = duckdb.connect()
-        >>> ed.dt.mimic_iv_omop(backend_handle=conn)
+        >>> con = duckdb.connect()
+        >>> ed.dt.mimic_iv_omop(backend_handle=con)
         >>> con.execute("SHOW TABLES;").fetchall()
     """
-    DATA_PATH = "ehrapy_data/mimic-iv-demo-data-in-the-omop-common-data-model-0.9"
-    if os.path.exists(DATA_PATH):
-        print(f"Load downloaded tables from {DATA_PATH}")
+    if data_path is None:
+        data_path = "ehrapy_data/mimic-iv-demo-data-in-the-omop-common-data-model-0.9"
+
+    if os.path.exists(data_path):
+        print(f"Load downloaded tables from {data_path}")
     else:
         print("Downloading data...")
         URL = "https://physionet.org/static/published-projects/mimic-iv-demo-omop/mimic-iv-demo-data-in-the-omop-common-data-model-0.9.zip"
@@ -76,29 +74,69 @@ def mimic_iv_omop(backend="duckdb", backend_handle=None) -> None:
             with zipfile.ZipFile(io.BytesIO(response.content)) as z:
                 # Extract all contents of the ZIP file
                 z.extractall("ehrapy_data")  # Specify the folder where files will be extracted
-                print(f"Download successful. ZIP file downloaded and extracted successfully to {DATA_PATH}.")
+                print(f"Download successful. ZIP file downloaded and extracted successfully to {data_path}.")
         else:
             print(f"Failed to download the file. Status code: {response.status_code}")
             return
 
-    return _set_up_duckdb(DATA_PATH + "/1_omop_data_csv", backend_handle)
+    return _set_up_duckdb(data_path + "/1_omop_data_csv", backend_handle)
 
 
-def gibleed_omop(backend="duckdb"):
-    """Loads the GIBleed dataset"""
+def gibleed_omop(backend_handle: DuckDBPyConnection, data_path: Path | None = None) -> None:
+    """Loads the GIBleed dataset.
+
+    More details: https://github.com/OHDSI/EunomiaDatasets.
+
+    Parameters
+    ----------
+        backend_handle: A handle to the backend which shall be used. Only duckdb connection supported at the moment.
+        data_path: Path to the tables. If the path exists, the data is loaded from there. Else, the data is downloaded.
+
+    Returns
+    -------
+    Returns nothing, but adds the tables to the backend via the handle.
+
+    Examples
+    --------
+        >>> import ehrapy as ep
+        >>> import ehrdata as ed
+        >>> import duckdb
+        >>> con = duckdb.connect()
+        >>> ed.dt.gibleed_omop(backend_handle=con)
+        >>> con.execute("SHOW TABLES;").fetchall()
+    """
     # TODO:
     # https://github.com/darwin-eu/EunomiaDatasets/tree/main/datasets/GiBleed
     raise NotImplementedError()
 
 
-def synthea27Nj_omop(backend="duckdb"):
-    """Loads the GIBleed dataset"""
+def synthea27nj_omop(backend_handle: DuckDBPyConnection, data_path: Path | None = None) -> None:
+    """Loads the Synthe27Nj dataset.
+
+    Parameters
+    ----------
+        backend_handle: A handle to the backend which shall be used. Only duckdb connection supported at the moment.
+        data_path: Path to the tables. If the path exists, the data is loaded from there. Else, the data is downloaded.
+
+    Returns
+    -------
+    Returns nothing, but adds the tables to the backend via the handle.
+
+    Examples
+    --------
+        >>> import ehrapy as ep
+        >>> import ehrdata as ed
+        >>> import duckdb
+        >>> con = duckdb.connect()
+        >>> ed.dt.synthea27nj_omop(backend_handle=con)
+        >>> con.execute("SHOW TABLES;").fetchall()
+    """
     # TODO
     # https://github.com/darwin-eu/EunomiaDatasets/tree/main/datasets/Synthea27Nj
     raise NotImplementedError()
 
 
-def mimic_2(backend="duckdb"):
+def mimic_2(backend_handle: DuckDBPyConnection, data_path: Path | None = None) -> None:
     """Loads the MIMIC2 dataset"""
     # TODO: replace mimic_2 as is in ehrapy with its dict-of-table return time - map variables to OMOP?
     raise NotImplementedError()
