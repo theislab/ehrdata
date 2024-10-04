d22e8f543045f1930426f92320b5bd830210410d
Eljas Roellin <eljas.roellin@ikmail.com>
2024-10-04 14:25:58 +0200

    omop io and some drafts

--- src/ehrdata/__init__.py
+++ src/ehrdata/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
+from . import dt, io, pl, pp, tl
 
-__all__ = ["pl", "pp", "tl"]
+__all__ = ["dt", "io", "pl", "pp", "tl"]
 
 __version__ = version("ehrdata")
---
+++ src/ehrdata/dt/__init__.py
@@ -0,0 +1,1 @@
+from .datasets import mimic_iv_omop
