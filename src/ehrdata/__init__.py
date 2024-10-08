8208c5d6c9bb1e6ff0925d7ebcb49c58f42dad37
Eljas Roellin <eljas.roellin@ikmail.com>
2024-10-08 16:15:44 +0200

    add EHRData class

--- src/ehrdata/__init__.py
+++ src/ehrdata/__init__.py
@@ -1,7 +1,9 @@
 from importlib.metadata import version
 
+from core import EHRData
+
 from . import dt, io, pl, pp, tl
 
-__all__ = ["dt", "io", "pl", "pp", "tl"]
+__all__ = ["EHRData", "dt", "io", "pl", "pp", "tl"]
 
 __version__ = version("ehrdata")
---
+++ src/ehrdata/core/ehrdata.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+
+class EHRData:
+    """EHRData class."""
+
+    pass
