51d2172c7492ef036b327bd4a9173bbfc0dd33a2
Eljas Roellin <eljas.roellin@ikmail.com>
2024-11-02 22:36:56 +0100

    stop there w/ tests for this PR

--- tests/test_dt/test_dt.py
+++ tests/test_dt/test_dt.py
@@ -10,15 +10,16 @@ def test_mimic_iv_omop():
     con.close()
 
 
-def test_gibleed_omop():
-    con = duckdb.connect()
-    ed.dt.gibleed_omop(backend_handle=con)
-    assert len(con.execute("SHOW TABLES").df()) == 36
-    con.close()
+# TODO
+# def test_gibleed_omop():
+#     con = duckdb.connect()
+#     ed.dt.gibleed_omop(backend_handle=con)
+#     assert len(con.execute("SHOW TABLES").df()) == 36
+#     con.close()
 
 
-def test_synthea27nj_omop():
-    con = duckdb.connect()
-    ed.dt.synthea27nj_omop(backend_handle=con)
-    assert len(con.execute("SHOW TABLES").df()) == 37
-    con.close()
+# def test_synthea27nj_omop():
+#     con = duckdb.connect()
+#     ed.dt.synthea27nj_omop(backend_handle=con)
+#     assert len(con.execute("SHOW TABLES").df()) == 37
+#     con.close()
