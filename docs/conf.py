53db3ffa2d200abba4127ca3bb76a042a4fe1bb0
Lukas Heumos <lukas.heumos@posteo.net>
2025-05-15 20:39:03 +0200

    sphinx nitpick
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- docs/conf.py
+++ docs/conf.py
@@ -144,6 +144,7 @@ pygments_style = "default"
 # If building the documentation fails because of a missing link that is outside your control,
 # you can add an exception to this list:
 nitpick_ignore = [
+    ("py:class", "pathlib._local.Path"),
     ("py:class", "types.EllipsisType"),
     # https://github.com/duckdb/duckdb-web/issues/3806
     ("py:class", "duckdb.duckdb.DuckDBPyConnection"),
