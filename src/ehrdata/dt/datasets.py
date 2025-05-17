cbfb263b98cd4c6049e1de65fac4d726c29e83c7
eroell <eljas.roellin@ikmail.com>
2025-05-17 13:53:36 +0200

    remove extra import

--- src/ehrdata/dt/datasets.py
+++ src/ehrdata/dt/datasets.py
@@ -6,7 +6,6 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 import pandas as pd
-from duckdb.duckdb import DuckDBPyConnection
 from lamin_utils import logger
 
 from ehrdata.dt.dataloader import download
