b4d19e5e42d9cbdf5217a8ca8c903fcb8e390cf1
eroell <eljas.roellin@ikmail.com>
2025-05-18 13:09:13 +0200

    debug

--- src/ehrdata/dt/dataloader.py
+++ src/ehrdata/dt/dataloader.py
@@ -43,7 +43,7 @@ def download(
         max_retries: Maximum number of download retries.
         retry_delay: Delay between retries in seconds.
     """
-    raw_formats = ["csv", "txt", "parquet", "TODO remove me"]
+    raw_formats = ["csv", "txt", "parquet"]
 
     def _sanitize_file_name(file_name: str) -> str:
         if os.name == "nt":
