288bcf157b4bbbac7bb2b167f9b083d71ff3a9c8
eroell <eljas.roellin@ikmail.com>
2025-06-18 16:53:03 +0200

    sparse io test files

--- tests/data/toy_zarr_h5ad_test_files_creation_reference.py
+++ tests/data/toy_zarr_h5ad_test_files_creation_reference.py
@@ -5,6 +5,7 @@ import anndata as ad
 import h5py
 import numpy as np
 import pandas as pd
+import scipy as sp
 import zarr
 
 #########################
@@ -21,13 +22,13 @@ adata_basic = ad.AnnData(
     varp={"var_level_connectivities": np.random.randn(4, 4)},
     uns={"information": ["info1"]},
 )
-adata_basic.write_zarr("toy_zarr/adata_basic.zarr")
-adata_basic.write_h5ad("toy_h5ad/adata_basic.h5ad")
+# adata_basic.write_zarr("toy_zarr/adata_basic.zarr")
+# adata_basic.write_h5ad("toy_h5ad/adata_basic.h5ad")
 
 #########################
-# adata_basic.zarr/h5ad:  basic EHRData object with tem, R
+# edata_basic_with_tem.zarr/h5ad:  basic EHRData object with tem, R
 
-zarr_file = zarr.open("toy_zarr/edata_basic_with_tem.zarr", mode="w")
+
 edata_basic_with_tem_dict = {
     "X": np.ones((5, 4)),
     "R": np.ones((5, 4, 2)),
@@ -41,10 +42,28 @@ edata_basic_with_tem_dict = {
     "uns": {"information": ["info1"]},
     "tem": pd.DataFrame({"timestep": ["t1", "t2"]}),
 }
-for k, v in edata_basic_with_tem_dict.items():
-    ad.io.write_elem(zarr_file, k, v)
+with zarr.open("toy_zarr/edata_basic_with_tem.zarr", "w") as zarr_file:
+    for k, v in edata_basic_with_tem_dict.items():
+        ad.io.write_elem(zarr_file, k, v)
 
 
 with h5py.File("toy_h5ad/edata_basic_with_tem.h5ad", "w") as h5ad_file:
     for k, v in edata_basic_with_tem_dict.items():
+        ad.io.write_elem(h5ad_file, k, v)
+
+
+#########################
+# edata_sparse_with_tem.zarr/h5ad:  basic EHRData object with tem, R, sparse X
+edata_sparse_with_tem_dict = edata_basic_with_tem_dict.copy()
+edata_sparse_with_tem_dict["X"] = sp.sparse.csr_matrix(edata_sparse_with_tem_dict["X"])
+edata_sparse_with_tem_dict["layers"]["other_layer"] = sp.sparse.csr_matrix(
+    edata_sparse_with_tem_dict["layers"]["other_layer"]
+)
+with zarr.open("toy_zarr/edata_sparse_with_tem.zarr", "w") as zarr_file:
+    for k, v in edata_sparse_with_tem_dict.items():
+        ad.io.write_elem(zarr_file, k, v)
+
+
+with h5py.File("toy_h5ad/edata_sparse_with_tem.h5ad", "w") as h5ad_file:
+    for k, v in edata_sparse_with_tem_dict.items():
         ad.io.write_elem(h5ad_file, k, v)
