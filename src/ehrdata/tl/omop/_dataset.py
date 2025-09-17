77cbc5f819e81f105836396ba91801a823090914
eroell <eljas.roellin@ikmail.com>
2025-09-17 21:35:22 +0200

    improve ci robustness fairlearn

--- src/ehrdata/tl/omop/_dataset.py
+++ src/ehrdata/tl/omop/_dataset.py
@@ -35,7 +35,7 @@ class EHRDataset(torch.utils.data.Dataset):
 
         Args:
             con: The connection to the database.
-            edata: Data object.
+            edata: Central data object.
             data_tables: The OMOP data tables to extract.
             target: The target variable to be used.
             datetime: If True, use datetime, if False, use date.
