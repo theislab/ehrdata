9f9bced0539325826861714445c864d3b32bbaa2
Öykü Süoglu <oykusuogluu@gmail.com>
2026-06-02 09:02:06 +0200

    fix in conf

--- docs/conf.py
+++ docs/conf.py
@@ -145,6 +145,7 @@ nitpick_ignore = [
     ("py:class", "zarr.core.buffer.core.Buffer"),
     ("py:class", "ehrdata._compat.ZappyArray"),
     ("py:class", "dask.array.core.Array"),
+    ("py:class", "dask.array.Array"),
     ("py:class", "anndata.compat.CupyArray"),
     ("py:class", "anndata.compat.CupySparseMatrix"),
     ("py:class", "sparse.numba_backend._coo.core.COO"),
